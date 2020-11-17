3a2fca74d694f5fdee82d5dc209af008925c626c
jsksru <mail@jsks.ru>
2020-11-17 17:00:11 +0500

    added add new button

--- src/layouts/Main/Main.js
+++ src/layouts/Main/Main.js
@@ -1,12 +1,33 @@
 import RequestsTable from '../../components/RequestsTable';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 
 const Main = () => {
   return (
-    <div>
-      <Typography variant="h4" component="h1">Список заявок</Typography>
-      <RequestsTable />
-    </div>
+    <>
+      <Grid container spacing={3} justify="space-between">
+        <Grid item>
+          <Typography variant="h4" component="h1">Список заявок</Typography>
+        </Grid>
+        <Grid item>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          startIcon={<AddIcon />}
+        >
+        Новая заявка
+      </Button>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <RequestsTable />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
