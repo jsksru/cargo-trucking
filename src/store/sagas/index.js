f6e67d197c9bf08f590c9048e3fff2ed46f7247c
jsksru <mail@jsks.ru>
2020-11-19 16:54:28 +0500

    index saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,49 +1,12 @@
-import { call, put, takeEvery } from 'redux-saga/effects';
-import { updateRequests, changeLoadingStatus, changeSavingStatus, loadRequestsTrigger } from '../actions/requests';
-import { errorEnable, changeErrorText, errorDisable } from '../actions/errors';
-import * as requestsApi from '../../api/requests';
+import { takeEvery } from 'redux-saga/effects';
+
 import { LOAD_REQUESTS_TRIGGER, NEW_REQUEST } from '../constants/requests';
 
-function* getRequests() {
-  yield put(changeLoadingStatus(true));
-  try {
-    const result = yield call(requestsApi.getAll);
-    if (result && result.length && result.length >= 1) {
-      yield put(errorDisable());
-      yield put(updateRequests(result));
-    } else {
-      yield put(changeErrorText('Таблица заявок пуста'));
-      yield put(errorEnable());
-    }
-  } catch(e) {
-    yield put(changeErrorText('Ошибка загрузки данных'));
-    yield put(errorEnable());
-  }
-  yield put(changeLoadingStatus(false));
-}
-
-function* newRequest(params) {
-  yield put(changeSavingStatus(true));
-  try {
-    const result = yield call(requestsApi.addNew, params.data);
-    if (result) {
-      yield put(loadRequestsTrigger());
-      yield put(changeSavingStatus(false));
-      params.historyRouter.push('/');
-    } else {
-      yield put(changeErrorText('Ошибка добавления данных'));
-      yield put(errorEnable());
-    }
-  } catch(e) {
-    yield put(changeErrorText('Ошибка добавления данных'));
-    yield put(errorEnable());
-  }
-  yield put(changeSavingStatus(false));
-}
+import * as requestSaga from './requests.saga';
 
 function* MySaga() {
-  yield takeEvery(LOAD_REQUESTS_TRIGGER, getRequests);
-  yield takeEvery(NEW_REQUEST, newRequest);
+  yield takeEvery(LOAD_REQUESTS_TRIGGER, requestSaga.getRequests);
+  yield takeEvery(NEW_REQUEST, requestSaga.newRequest);
 }
 
 export default MySaga;
