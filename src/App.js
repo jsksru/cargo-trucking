9734155522ae6beaa6526c6e0dcdaea18b868b36
jsksru <mail@jsks.ru>
2020-11-20 20:36:04 +0500

    added codesplitting

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
+import { Suspense, lazy } from 'react';
 import { Container } from '@material-ui/core';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AppHeader from './components/AppHeader';
-import Main from './layouts/Main';
-import Request from './layouts/Request';
-import NewRequest from './layouts/NewRequest';
-import RequestEdit from './layouts/RequestEdit';
+import Loader from './components/Loader';
+
+const Main = lazy(()=>import('./layouts/Main'));
+const Request = lazy(()=>import('./layouts/Request'));
+const NewRequest = lazy(()=>import('./layouts/NewRequest'));
+const RequestEdit = lazy(()=>import('./layouts/RequestEdit'));
 
 function App() {  
   return (
     <Router>
       <Container>
         <AppHeader />
-        <Switch>
-          <Route exact path="/">
-            <Main />
-          </Route>
-          <Route path="/info/:id">
-            <Request />
-          </Route>
-          <Route path="/edit/:id">
-            <RequestEdit />
-          </Route>
-          <Route path="/new">
-            <NewRequest />
-          </Route>
-        </Switch>
+        <Suspense fallback={<Loader/>}>
+          <Switch>
+            <Route exact path="/">
+              <Main />
+            </Route>
+            <Route path="/info/:id">
+              <Request />
+            </Route>
+            <Route path="/edit/:id">
+              <RequestEdit />
+            </Route>
+            <Route path="/new">
+              <NewRequest />
+            </Route>
+          </Switch>
+        </Suspense>
       </Container>
     </Router>
   );
