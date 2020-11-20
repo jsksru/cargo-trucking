c2f266fd942ba20ec5760ac14b75c6543bf0a269
jsksru <mail@jsks.ru>
2020-11-20 17:03:38 +0500

    added links to edit and view

--- src/components/RequestsTable/RequestsTable.js
+++ src/components/RequestsTable/RequestsTable.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 import Table from '@material-ui/core/Table';
-import Link from '@material-ui/core/Link'
+import StaticLink from '@material-ui/core/Link'
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
@@ -59,13 +60,13 @@ const RequestsTable = () => {
         <TableCell align="left">{row.carrier}</TableCell>
         <TableCell align="left">{formatPhoneNumber(row.phone)}</TableCell>
         <TableCell align="left">
-          <Link href={`https://ati.su/firms/${row.code}/info`}>{row.code}</Link>
+          <StaticLink href={`https://ati.su/firms/${row.code}/info`} target="_blank">{row.code}</StaticLink>
         </TableCell>
         <TableCell align="right">
-          <IconButton aria-label="View">
+          <IconButton aria-label="View" component={Link} to={`./info/${row.id}`}>
             <VisibilityIcon />
           </IconButton>
-          <IconButton aria-label="Edit" color="primary">
+          <IconButton aria-label="Edit" color="primary" component={Link} to={`./edit/${row.id}`}>
             <EditIcon />
           </IconButton>
           <DeleteButton id={row.id}/>
