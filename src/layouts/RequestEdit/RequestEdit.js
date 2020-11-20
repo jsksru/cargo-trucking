0721e7d37bcfe8feda4459244e5c913a4f7fe4d6
jsksru <mail@jsks.ru>
2020-11-20 20:34:06 +0500

    added request edit page

--- src/layouts/RequestEdit/RequestEdit.js
+++ src/layouts/RequestEdit/RequestEdit.js
@@ -1,12 +1,141 @@
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from  '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import KeyboardReturnIcon from '@material-ui/icons/KeyboardReturn';
+import { Link, useHistory, useParams } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import api from '../../api';
+import ClientSelector from '../../components/ClientSelector';
+import CarrierSelector from '../../components/CarrierSelector';
+import TableContainer from '@material-ui/core/TableContainer';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Loader from '../../components/Loader';
+import DateTimeSelector from '../../components/DateTimeSelector';
 
-const RequestEdit = () => {
+const NewRequest = () => {
   const params = useParams();
-  return (
-    <div>
-      edit : {params.id}
-    </div>
+  const [ loading, setLoading ] = useState(true);
+  const [ requestId, setRequestId ] = useState(null);
+
+  const [ datetime, setDatetime ] = useState(null);
+  const [ client, setClient ] = useState(null);
+  const [ carrier, setCarrier ] = useState(null);
+  const [ comments, setComments ] = useState('');
+  const [ isSaving, setIsSaving ] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    setLoading(true);
+    api.requests.getOne(parseInt(params.id))
+      .then(result => {
+        if (result) {
+          setRequestId(result.id)
+          setDatetime(result.datetime);
+          setClient(result.client.id);
+          setCarrier(result.carrier.id);
+          setComments(result.comments)
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [params.id]);
+  
+
+  const handleCreateButton = () => {
+    setIsSaving(true);
+      api.requests.addNew({
+      client,
+      carrier,
+      comments,
+    }).then(id => {
+      if (id) {
+        setIsSaving(false);
+        history.push('/');
+      }
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  return loading ? <Loader />: (
+    <>
+    <Grid container spacing={3} direction="column">
+      <Grid item>
+        <Button component={Link} to="/" variant="outlined" size="small" startIcon={<KeyboardReturnIcon />}>Назад к списку заявок</Button>
+      </Grid>
+      <Grid item>
+        <Typography variant="h4" component="h1">Редактирование заявки № {requestId}</Typography>
+      </Grid>
+    </Grid>
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <TableContainer>
+          <Table aria-label="simple table">
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="button">Дата и время создания</Typography>
+                </TableCell>
+                <TableCell>
+                  <DateTimeSelector initValue={datetime} handler={setDatetime} />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="button">Фирма клиента</Typography>
+                </TableCell>
+                <TableCell>
+                  <ClientSelector selectHandler={setClient} />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="button">Перевозчик</Typography>
+                </TableCell>
+                <TableCell>
+                  <CarrierSelector selectHandler={setCarrier} />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="button">Комментарий</Typography>
+                </TableCell>
+                <TableCell>
+                  <TextField fullWidth
+                    id="outlined-multiline-static"
+                    label="Комментарии"
+                    multiline
+                    rows={5}
+                    variant="outlined"
+                    value={comments}
+                    onInput={(e) => setComments(e.target.value)}
+                  />
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container justify="flex-end">
+          {
+            isSaving ?
+              <Button variant="contained" color="primary" size="large" disabled>Сохранение...</Button> :
+              <Button variant="contained" color="primary" size="large" onClick={handleCreateButton} disabled={!(client&&carrier&&datetime)}>Сохранить изменения</Button>
+          }
+        </Grid>
+      </Grid>
+    </Grid>
+    </>
   );
 };
 
-export default RequestEdit;
+export default NewRequest;
