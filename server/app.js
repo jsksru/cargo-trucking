9c9b4c698fdfb3f89896623725051203dbba09cf
jsksru <mail@jsks.ru>
2020-11-21 19:08:10 +0500

    added cors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const { json } = require('express');
 const express = require('express');
 const App = express();
+const cors = require('cors');
 const CarriersRouter = require('./routes/carriers');
 
 App.use(json());
+App.use(cors());
 
 App.use('/carriers', CarriersRouter);
 
