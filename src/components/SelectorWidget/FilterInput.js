91be756fbc0fcfd2411f9c8204c7426ae1daeeb6
jsksru <mail@jsks.ru>
2020-11-21 09:34:30 +0500

    removed uniq generator fix

--- src/components/SelectorWidget/FilterInput.js
+++ src/components/SelectorWidget/FilterInput.js
@@ -1,12 +1,11 @@
 import TextField from '@material-ui/core/TextField';
-import { generateRandomForTextFieldIDs } from '../../utils';
 
 const FilterInput = ({ handler }) => {
 
   const inputHandler = (e) => handler(e.target.value);
 
   return (
-    <TextField id={`text-field-${generateRandomForTextFieldIDs()}`} label="Поиск..." variant="outlined" onInput={inputHandler} fullWidth autoComplete="false" />
+    <TextField id="widget-filter-input" label="Поиск..." variant="outlined" onInput={inputHandler} fullWidth autoComplete="false" />
   )
 };
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -6,11 +6,3 @@ export const formatDateTime = (datetime) => {
 export const formatPhoneNumber = (phone) => {
   return phone.replace(/7(\d{3})(\d{2})(\d{2})(\d{3})/,'($1) $2-$3-$4');
 };
-
-export const generateRandomForTextFieldIDs = () => {
-  function randomInteger(min, max) {
-    let rand = min - 0.5 + Math.random() * (max - min + 1);
-    return Math.round(rand);
-  }
-  return randomInteger(10000, 99999);
-};
