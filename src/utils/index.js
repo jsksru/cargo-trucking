c555c4372caf2018a72f21ca78d0295f815e814b
jsksru <mail@jsks.ru>
2020-11-21 10:55:59 +0500

    added similar helper

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,11 @@ export const formatDateTime = (datetime) => {
   return dt.toLocaleDateString('ru-RU', { day: 'numeric', month: 'short' }) + ' ' + dt.toLocaleTimeString('ru-RU', { hour: 'numeric', minute: '2-digit' });
 };
 
+export const formatDateTimeFull = (datetime) => {
+  const dt = new Date(datetime);
+  return dt.toLocaleDateString('ru-RU', { day: 'numeric', month: 'long' }) + ' в ' + dt.toLocaleTimeString('ru-RU', { hour: 'numeric', minute: '2-digit' });
+};
+
 export const formatPhoneNumber = (phone) => {
   return phone.replace(/7(\d{3})(\d{2})(\d{2})(\d{3})/,'($1) $2-$3-$4');
 };
