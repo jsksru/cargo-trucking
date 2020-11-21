7f4011f3c28a165c8dee8e429f862251673a7027
jsksru <mail@jsks.ru>
2020-11-21 19:14:26 +0500

    added logs

--- server/server.js
+++ server/server.js
@@ -1,4 +1,13 @@
 const http = require('http');
 const App = require('./app');
 
-http.createServer(App).listen(8080);
+const PORT = 8080;
+
+http.createServer(App)
+  .listen(PORT)
+  .on('listening', () => {
+    console.log(`Server listening on ${PORT} port!`);
+  })
+  .on('error', (err) => {
+    console.log('SERVER ERROR:', err);
+  });
