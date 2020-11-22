6afea9b2fba727981ad1db599b4b681d7481653b
jsksru <mail@jsks.ru>
2020-11-22 18:45:49 +0500

    added sort and search

--- server/controllers/requests/all.js
+++ server/controllers/requests/all.js
@@ -1,9 +1,20 @@
 const fs = require('fs').promises;
+const { TableBody } = require('@material-ui/core');
 const path = require('path');
 
 const FILE_NAME = path.resolve('server/data/','requests.json');
 const CLIENTS_FILE = path.resolve('server/data/','clients.json');
 const CARRIERS_FILE = path.resolve('server/data/','carriers.json');
+
+const sortData = (array, field, reverse = false) => {
+  const sortByField = (field) => {
+    if (reverse) {
+      return (a, b) => a[field] < b[field] ? 1 : -1;
+    }
+    return (a, b) => a[field] > b[field] ? 1 : -1;
+  }
+  return [...array].sort(sortByField(field));
+};
 
 module.exports = async(req, res) => {
   try {
@@ -15,7 +26,23 @@ module.exports = async(req, res) => {
     const clients = JSON.parse(clientsFileData);
     const carriers = JSON.parse(carriersFileData);
 
-    const mappedData = requests.map(item => {
+    let sortedData = requests;
+    
+    if (req.query && req.query.sort) {
+      if (req.query.reverse && req.query.reverse === 'true') {
+        sortedData = sortData(sortedData, req.query.sort, true);
+      } else {
+        sortedData = sortData(sortedData, req.query.sort);
+      }
+    }
+
+    if (req.query && req.query.search) {
+      sortedData = sortedData.filter(item => {
+        return item.comments.toLowerCase().indexOf(req.query.search.toLowerCase()) !== -1;
+      });
+    }
+
+    const mappedData = sortedData.map(item => {
       const client = clients.find(i => i.id === item.client);
       const carrier = carriers.find(i => i.id === item.carrier);
 
