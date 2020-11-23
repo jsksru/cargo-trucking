cdc637ffb6ddb41192f7505395affeacaf8b2917
jsksru <mail@jsks.ru>
2020-11-23 12:07:33 +0500

    moved state to up component

--- src/layouts/Main/Main.js
+++ src/layouts/Main/Main.js
@@ -1,41 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import RequestsTable from '../../components/RequestsTable';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import { Link } from 'react-router-dom';
 import Loader from '../../components/Loader';
-import api from '../../api';
+import TableFilters from '../../components/TableFilters';
+import OutData from '../../components/OutData';
+
+import { loadRequestsAsync } from '../../store/actions';
 
 const Main = () => {
-  const [ loading, setLoading ] = useState(true);
-  const [ requests, setRequests ] = useState([]);
-  const [ sortField, setSortField ] = useState('id');
-  const [ sortReverse, setSortReverse ] = useState(false);
-  const [ search, setSearch ] = useState('');
-  const [ searchView, setSearchView ] = useState('');
-
-  const updateRequests = async() => {
-    setLoading(true);
-    try {
-      const dataFromServer = await api.requests.getAll({
-        sort: sortField,
-        reverse: sortReverse,
-        search: search || null,
-      });
-      if (dataFromServer) setRequests(dataFromServer);
-    } catch (err) {
-      console.log(err);
-    }
-    setLoading(false);
-  };
-  window['__FORCE_TABLE_UPDATE'] = updateRequests;
+  const loading = useSelector(state => state.loading);
+  const requests = useSelector(state => state.requests);
+  const filters = useSelector(state => state.filters);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    updateRequests();
-  }, [sortField, sortReverse, search]);
+    dispatch(loadRequestsAsync());
+  }, [filters]);
 
   return (
     <>
@@ -44,60 +29,26 @@ const Main = () => {
           <Typography variant="h4" component="h1">Список заявок</Typography>
         </Grid>
         <Grid item>
-        <Button
-          component={Link}
-          to="/new"
-          variant="contained"
-          color="primary"
-          size="large"
-          startIcon={<AddIcon />}
-        >
-        Новая заявка
-      </Button>
+          <Button
+            component={Link}
+            to="/new"
+            variant="contained"
+            color="primary"
+            size="large"
+            startIcon={<AddIcon />}
+          >Новая заявка</Button>
         </Grid>
       </Grid>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Button
-            onClick={()=>{
-              setSortField('id');
-              setSortReverse(!sortReverse);
-            }}
-            variant="contained"
-            size="small"
-          >Сортировка по id {sortReverse? '↑':'↓'}</Button>
-          <Button
-            onClick={()=>{
-            setSortField('datetime');
-            setSortReverse(!sortReverse);
-            }}
-            variant="contained"
-            size="small"
-          >Сортировка по Дате и времени {sortReverse? '↑':'↓'}</Button>
-          <TextField
-            variant="outlined"
-            label="Поиск в комментариях"
-            id="comment-search"
-            value={searchView}
-            size="small"
-            onInput={(e)=>{
-              setSearchView(e.target.value);
-              setTimeout(()=>{
-                if (searchView.trim().length > 1) {
-                  setSearch(searchView);
-                } else {
-                  setSearch('');
-                }
-              }, 1000);
-            }}
-          />
+          <TableFilters />
         </Grid>
         <Grid item xs={12}>
           {
             loading ? <Loader/> :
             requests && requests.length && requests.length > 0 ?
             <RequestsTable items={requests}/>:
-            <div>Нет данных!</div>
+            <OutData />
           }
         </Grid>
       </Grid>
